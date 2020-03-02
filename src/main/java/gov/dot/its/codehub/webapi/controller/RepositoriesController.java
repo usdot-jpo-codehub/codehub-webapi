52f111311d5a7c20433e888a10485787054004bf
Gio <rodriguez_carlos@bah.com>
2020-03-02 16:58:18 -0500

    Enabling CORS control capabilities.

--- src/main/java/gov/dot/its/codehub/webapi/controller/RepositoriesController.java
+++ src/main/java/gov/dot/its/codehub/webapi/controller/RepositoriesController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import gov.dot.its.codehub.webapi.model.CHRepository;
 import gov.dot.its.codehub.webapi.model.CHSearchRequest;
 import gov.dot.its.codehub.webapi.service.RepositoriesService;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class RepositoriesController {
 
