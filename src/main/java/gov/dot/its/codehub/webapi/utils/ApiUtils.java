75c369fb2719e98529e3701155fff9d9948393b6
Gio <rodriguez_carlos@bah.com>
2020-07-16 12:04:02 -0400

    RDAODTD-1973: Fix invoke method only conditionally

--- src/main/java/gov/dot/its/codehub/webapi/utils/ApiUtils.java
+++ src/main/java/gov/dot/its/codehub/webapi/utils/ApiUtils.java
@@ -5,6 +5,7 @@ import java.security.NoSuchAlgorithmException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import gov.dot.its.codehub.webapi.model.ApiError;
 
@@ -94,17 +96,41 @@ public class ApiUtils {
 	}
 
 	public List<ApiError> getErrorsFromException(List<ApiError> errors, Exception e) {
-		errors.add(new ApiError(String.format(MESSAGE_TEMPLATE, ERROR_LABEL, e.getMessage())));
-		logger.error(String.format(MESSAGE_TEMPLATE, ERROR_LABEL, e.getMessage()));
+		String msg = stringFormat(MESSAGE_TEMPLATE, ERROR_LABEL, e.getMessage());
+		errors.add(new ApiError(msg));
+		logger.error(msg);
 		if (debug) {
-			logger.error(String.format(MESSAGE_TEMPLATE, ERROR_LABEL, e.toString()));
+			msg = stringFormat(MESSAGE_TEMPLATE, ERROR_LABEL, e.toString());
+			logger.error(msg);
 		}
 		if (e.getSuppressed().length > 0) {
 			for (Throwable x : e.getSuppressed()) {
-				errors.add(new ApiError(String.format(MESSAGE_TEMPLATE, ERROR_LABEL, x.toString())));
-				logger.error(String.format(MESSAGE_TEMPLATE, ERROR_LABEL, x.toString()));
+				msg = stringFormat(MESSAGE_TEMPLATE, ERROR_LABEL, x.toString());
+				errors.add(new ApiError(msg));
+				logger.error(msg);
 			}
 		}
 		return errors;
+	}
+
+	public String stringFormat(String template, Object... values) {
+		int n = StringUtils.countOccurrencesOf(template, "%s");
+		if (n == values.length) {
+			return String.format(template, values);
+		}
+		Object[] result = new Object[n];
+		Arrays.fill(result, "");
+		if (n > values.length) {
+			System.arraycopy(values, 0, result, 0, values.length);
+			return String.format(template, result);
+		}
+
+		System.arraycopy(values, 0, result, 0, result.length);
+		StringBuilder s = new StringBuilder();
+		for(int i=n; i<values.length; i++) {
+			s.append(values[i] == null ? "": values[i] + " ");
+		}
+		result[result.length-1] = (result[result.length-1] == null ? "" : result[result.length-1])+ " " +s.toString();
+		return String.format(template, result);
 	}
 }
